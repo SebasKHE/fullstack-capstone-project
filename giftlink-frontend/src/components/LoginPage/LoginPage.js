7174b148ae72274ec1dde5a7b6f201367913f3d2
SebasKHE <sebasttcaicedo@gmail.com>
2025-02-27 15:37:40 -0500

    Added Login and Register components

---
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -0,0 +1,43 @@
+import React, {use, useState} from 'react'; 
+import './loginPage.css';
+
+function LoginPage() {
+    const {email, setEmail} = useState('');
+    const {password, setPassword} = useState('');
+    
+    const handleLogin = () => {
+        console.log('Inside handleLogin');
+    }
+
+    return (
+        <div className='container mt-5'>
+            <div className='row justify-content-center'>
+                <div className='col-md-6 col-lg-4'>
+                    <div className='login-card p-4 border rounded'>
+                        <h2 className='text-center mb-4 font-weight-bold'>Login</h2>
+                        <div className='mb-3'>
+                            <label htmlFor='email' className='form-label'>Email</label>
+                            <input 
+                            type='text' 
+                            className='form-control' 
+                            id='email' 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}/>
+                            <label htmlFor='password' className='form-label'>Password</label>
+                            <input 
+                            type='password' 
+                            className='' 
+                            id='password' 
+                            name='password'
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}/>
+                        </div>
+                        <button className='btn btn-primary w-100 mb-3' onClick={handleLogin}>Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default LoginPage;
