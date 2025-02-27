196707836783a29bbf929597ba3946a3a880b4c1
SebasKHE <sebasttcaicedo@gmail.com>
2025-02-27 09:49:03 -0500

    debugged last commit

--- sentiment/index.js
+++ sentiment/index.js
@@ -18,7 +18,7 @@ app.use(expressPino);
 app.post('/sentiment', async (req, res) => {
 
     // Task 4: extract the sentence parameter
-    const { sentence } = req.query.sentence;
+    const { sentence } = req.query;
 
 
     if (!sentence) {
@@ -38,17 +38,17 @@ app.post('/sentiment', async (req, res) => {
         let sentiment = "neutral";
 
         // Task 5: set sentiment to negative or positive based on score rules
-        if (analysisResult > 0.5) {
-            sentiment = 'positive';
-        } else {
+        if (analysisResult < 0) {
             sentiment = 'negative';
+        } else if (analysisResult > 0.33) {
+            sentiment = 'positive';
         }
 
         // Logging the result
         logger.info(`Sentiment analysis result: ${analysisResult}`);
 
         // Task 6: send a status code of 200 with both sentiment score and the sentiment txt in the format { sentimentScore: analysisResult, sentiment: sentiment }
-        res.status(200).json({seentimentScore: analysisResult, sentiment: sentiment})
+        res.status(200).json({sentimentScore: analysisResult, sentiment: sentiment})
     } catch (error) {
         logger.error(`Error performing sentiment analysis: ${error}`);
         // Task 7: if there is an error, return a HTTP code of 500 and the json {'message': 'Error performing sentiment analysis'}
